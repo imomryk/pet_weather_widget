abf80d495930f63d0d204c433a68503154b3a504
imomryk <i.momryk@gmail.com>
2021-10-28 00:30:32 +0300

    api change

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
 const weather = document.getElementById("main_weather")
-let query = "http://api.weatherapi.com/v1/current.json?key=4dc742e4282c49a3a2d175330212110&"
+let query = "https://api.openweathermap.org/data/2.5/onecall?exclude=hourly,minutely,alerts&appid=c4c979e5736f06ccaf01358f455ee24e&"
 const days =["Sunday","Monday","Thuesday","Wednesday","Thursday","Friday","Saturday",]
-navigator.geolocation.getCurrentPosition(e => {
-    query += `q=${e.coords.latitude},${e.coords.longitude}`
-    console.log(e)
-    fetch(query)
-        .then(response => response.json())
-        .then(response2 => {
-            console.log(response2)
-            const date = new Date()
-            document.getElementById("date_day").innerText = `${date.getDate()}.${date.getMonth()+1}.${date.getFullYear()}`
-            document.getElementById("date_dayname").innerText = `${days[date.getDay()]}`
-            document.getElementById("search").value = `${response2.location.name}`
-        })
-})
-fetch("https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=AIzaSyB2oyG_Z1UcjXZ1IyUXa_VpgMhIGsggLxY")
-.then(r=>console.log(r.json()))
 
+
+
+navigator.geolocation.getCurrentPosition(e => {
+    let latitude = e.coords.latitude
+    let longitude = e.coords.longitude
+    fetch(`https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&apiKey=24b0b062d3c94efaab4bcca77b291472`,{method: 'GET',})
+    .then(response => response.json())
+    .then(resultReverseGeo => {
+        let currCity = resultReverseGeo.features[0].properties.city
+        document.getElementById("search").value = `${currCity}`
+        query += `lat=${latitude}&lon=${longitude}`
+        
+        
+        fetch(query)
+        .then(response => response.json())
+        .then(resultWeather => {
+            console.log(resultWeather)
+            console.log(resultWeather.current.dt)
+            const currDate = new Date(resultWeather.current.dt)
+            console.log(currDate)
+            document.getElementById("date_day").innerText = `${currDate.getDate()}.${currDate.getMonth()+1}.${currDate.getFullYear()}`
+            document.getElementById("date_dayname").innerText = `${days[currDate.getDay()]}`
+        })
+        .catch(err=>console.log("err:",err))
+    })
+    .catch(error => console.log('error', error));
+    
+})
 
